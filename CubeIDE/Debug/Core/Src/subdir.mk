7fa9a1921fa2e9e72768aee33696d1e897367e64
Hoàng Sỹ Xuân Sơn <son.hoang0852@hcmut.edu.vn>
2025-10-08 22:43:54 +0700

    update

--- CubeIDE/Debug/Core/Src/subdir.mk
+++ CubeIDE/Debug/Core/Src/subdir.mk
@@ -5,7 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/exercise6.c \
+../Core/Src/exercise7.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
@@ -14,7 +14,7 @@ C_SRCS += \
 ../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
-./Core/Src/exercise6.o \
+./Core/Src/exercise7.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
@@ -23,7 +23,7 @@ OBJS += \
 ./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
-./Core/Src/exercise6.d \
+./Core/Src/exercise7.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
